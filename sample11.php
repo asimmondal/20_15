1f37d95071a5ef2fbf8bed520cfa1a25662aba97
Mondal Asim <mandaru.asim@gmail.com>
2015-05-20 05:59:03 +0000

    add index

---
+++ sample11.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $age =27;
+    if ($age <=15){
+        
+        print('you are adult');
+        
+    }else{
+        print ('you are child');
+        
+    }
+    
+    ?>
+    
+    
+    
+</body>
+</html>
